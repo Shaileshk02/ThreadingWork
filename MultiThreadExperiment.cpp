89ecbb366bf698169353e0a436d25f534539d929
Shaileshk02 <shaileshpkulkarni@gmail.com>
2026-06-19 19:22:19 +0530

    Add files via upload

---
+++ MultiThreadExperiment.cpp
@@ -0,0 +1,50 @@
+// MultiThreadExperiment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+
+
+void threadFunction1() {
+    
+	for (size_t i = 0; i < 2000; i++)
+	{
+		std::cout << "999";
+	}
+	return;
+}
+
+
+void threadFunction2(char symbol) {
+
+	for (size_t i = 0; i < 2000; i++)
+	{
+		std::cout << symbol;
+	}
+	return;
+}
+
+
+int main()
+{
+    std::cout << "Let's start multi-threading!\n";
+	std::thread worker2(threadFunction2, '/');
+	std::thread worker1(threadFunction1);
+	
+	
+	worker1.join();  // Wait for the second thread to finish
+	worker2.join();
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
