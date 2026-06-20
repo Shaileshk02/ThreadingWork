77854d1c0b913d6fcf2ac2d6f7c6ee4bb3307b18
Shaileshk02 <shaileshpkulkarni@gmail.com>
2026-06-20 07:57:29 +0530

    updated comments and parameters
    updated comments and parameters with threads using functors

--- MultiThreadExperiment.cpp
+++ MultiThreadExperiment.cpp
@@ -3,8 +3,11 @@
 
 #include <iostream>
 #include <thread>
-
-
+// Read me:
+// In this a thread is created using function pointer and the thread is 
+// executed by calling the function in the thread. The main thread waits 
+// for the worker thread to finish before exiting. Also parameter is passed
+// to the thread function.
 void threadFunction1() {
     
 	for (size_t i = 0; i < 2000; i++)
@@ -37,14 +40,3 @@ int main()
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
