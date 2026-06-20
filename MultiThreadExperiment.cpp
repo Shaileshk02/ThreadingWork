b95e68816656530407afb5f645a60c16e946b8d6
Shaileshk02 <shaileshpkulkarni@gmail.com>
2026-06-20 13:08:26 +0530

    ThreadCallMethods
    It describes 5 ways thread can be called for different type of functions

--- MultiThreadExperiment.cpp
+++ MultiThreadExperiment.cpp
@@ -28,15 +28,78 @@ void threadFunction2(char symbol) {
 }
 
 
+//=== Functor class to be used for creating a thread
+class Functor {
+public:
+	void operator()(int count1) {
+		for(size_t i = 0; i< count1; i++)
+		{
+			std::cout << "Functor thread: " << i << std::endl;
+		}
+	}
+};
+
+class foo {
+
+public:
+	void runThread(int count2) {
+		for (size_t i = 0; i < count2; i++)
+		{
+			std::cout << "Non-static member function thread: " << i << std::endl;
+		}
+	}
+
+	static void staticFunction(int count3) {
+		
+		for(size_t ii = 0; ii< count3; ii++)
+		{
+			std::cout << "Static member function thread: " << ii << std::endl;
+		}
+		
+	}
+};
+
 int main()
 {
-    std::cout << "Let's start multi-threading!\n";
-	std::thread worker2(threadFunction2, '/');
-	std::thread worker1(threadFunction1);
+    
 	
+	std::cout << "Let's start multi-threading!\n";
 	
-	worker1.join();  // Wait for the second thread to finish
-	worker2.join();
+	//=== Method 1 : Create a thread using function pointer and pass parameter to the thread function
+	//std::thread worker2(threadFunction2, '/');
+	//std::thread worker1(threadFunction1);
+	//
+	//
+	//worker1.join();  // Wait for the second thread to finish
+	//worker2.join();
+
+	//===
+
+	//=== Method 2 : Create a thread using lambda function and pass parameter to the thread function
+	std::thread t([](int count) {
+		for (int i = 0; i < count; ++i) {
+			std::cout << "Lambda thread: " << i << std::endl;
+		}
+		}, 10);
+
+	t.join();  // Wait for the lambda thread to finish
+	//===
+
+	//=== Method 3 : Create a thread using functor object and pass parameter to the thread function
+	Functor functorObj;
+	std::thread t2(functorObj, 15);
+	t2.join();  // Wait for the functor thread to finish
+	//===
+
+	//=== Method 4 : Create a thread using a non-static member function of a class
+		foo fooObj;
+		std::thread t3(&foo::runThread, &fooObj, 11);
+		t3.join();  // Wait for the non-static member function thread to finish
+	//===
+	//=== Method 5 : Create a thread using a static member function of a class
+		std::thread t4(&foo::staticFunction, 12);
+		t4.join();  // Wait for the static member function thread to finish
+	//===
 
 	return 0;
 }
